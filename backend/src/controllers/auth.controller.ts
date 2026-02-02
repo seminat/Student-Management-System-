6804e19a1e190d00690d3d547951d9b89f2d37da
Seminat Aschalew <seminataschalew17@gmail.com>
2026-02-02 06:02:48 -0800

    add authentication controller with necessary imports

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import prisma from '../utils/prisma';
+import { AuthenticatedRequest } from '../types';
